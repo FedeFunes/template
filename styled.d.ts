59b4a61215c6adf919723b33849c397aa245b8db
FedeFunes <federicofunes92@gmail.com>
2025-03-11 14:51:05 -0300

    wip

--- styled.d.ts
+++ styled.d.ts
@@ -2,6 +2,6 @@ import "styled-components";
 import { IDefaultTheme } from "@fravega-it/bumeran-ds-fvg";
 
 declare module "styled-components" {
-  // eslint-disable-next-line
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends IDefaultTheme {}
 }
